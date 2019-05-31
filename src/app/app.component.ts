db2dc4c0548835da4599745890710284471edf0f
williamsoftwarecode <williamsoftwarecode@gmail.com>
2019-05-31 23:44:12 +0800

    Added Navigation for headers (Recipe and Shopping List) with Event Binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+  loadedFeature = 'recipe';
   name = 'Angular';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
